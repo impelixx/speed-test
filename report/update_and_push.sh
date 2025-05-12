88b8643da8a440127f41a119fec6e4c1f3014f28
root <root@less-east.yeezyhost.net>
2025-05-12 19:08:02 +0100

    auto: update charts

--- report/update_and_push.sh
+++ report/update_and_push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull
-python3 report/run_tests.py
-python3 report/generate_report.py
+python3 run_tests.py
+python3 generate_report.py
 git add README.md report/*.png
 git commit -m 'auto: update charts'
 git push
